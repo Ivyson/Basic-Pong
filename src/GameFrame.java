f200184265a171bbf69556ba98f24fcfe9420b23
82Sam <53128739+Ivyson@users.noreply.github.com>
2024-04-29 01:36:28 +0200

    Updated the files of my old pong game designed in Java

---
+++ src/GameFrame.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	GamePanel panel;
+	
+	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setLocationRelativeTo(null); // Ensure that the start is in the centre 
+		this.setVisible(true);
+	}
+}
---
+++ src/PongGame.java
@@ -0,0 +1,8 @@
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame(); //Calls the class to create a frame of the game
+		
+	}
+}
